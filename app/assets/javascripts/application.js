f10b48eb6ded9c369871be8be905f7c6888b28f6
nommy <nomuramasam@gmail.com>
2019-02-14 00:19:04 +0900

    JSとPHPも貼り付けた

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,92 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
-// vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require rails-ujs
-//= require activestorage
-//= require turbolinks
-//= require_tree .
+// ストレージに追加
+var saveStorage = function(Key,name,star){
+  var Value = {name: name, star: star}; // オブジェクトを作る
+  var setjson = JSON.stringify(Value); //JSON形式にエンコード
+  if (Key && Value.name && Value) {
+    localStorage.setItem(Key, setjson); //ストレージへ追加
+  }
+  Key = ''; Value = []; // 初期化  これでif(Value)に引っ掛かるのか心配
+  viewStorage();
+}
+
+// スターの値を変える
+var changeStar = function(id){
+  var getjson = localStorage.getItem(id); // 受け取ったidの行を選択
+  var obj = JSON.parse(getjson); // JSONをオブジェクトに（中身を見る為）
+  if(obj.star == 0){var _star = 1} else{var _star = 0}  // スター変更
+  var _id = id; // idそのまま
+  var _name = obj.name; // 単語名そのまま
+  saveStorage(_id,_name,_star); //あとは追加の時と同じ
+}
+
+// 特定のワードを削除
+var removeStorage = function(id){
+    //var id = document.getElementById("id").value;
+    localStorage.removeItem(id);
+    id = '';
+    viewStorage();
+};
+
+// 全て削除
+var clearStorage = function(){
+    localStorage.clear();
+    viewStorage();
+};
+
+// ユニークなIDを生成
+function getUniqueStr(myStrong){
+ var strong = 1000;
+ if (myStrong) strong = myStrong;
+ return new Date().getTime().toString(16)  + Math.floor(strong*Math.random()).toString(16)
+}
+
+// ローカルストレージのデータを表に出力
+var viewStorage = function(){
+    var tb = document.getElementById('tb')
+
+    // テーブルの初期化
+    while (tb.firstChild){
+      tb.removeChild(tb.firstChild);
+    }
+    // }
+    // テーブルの出力
+    for (var i=localStorage.length-1; i>=0; i--) {
+        var id = localStorage.key(i);
+        var getjson = localStorage.getItem(id); 
+        var obj = JSON.parse(getjson); // JSON → オブジェクト
+        var name = obj.name;
+        var star = obj.star;
+
+        var tr = document.createElement('tr');
+        var td1 = document.createElement('td');
+        var td2 = document.createElement('td');
+        var td3 = document.createElement('td');
+        tb.appendChild(tr);
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+        // スター  
+        if(star == 0){var color='light'} else {var color='yellow'}
+        td1.innerHTML = '<a onclick="changeStar(\'' + id + '\')" class="point '+ color +'"><i class="material-icons">star</i></a>';
+        // 検索ワード
+        td2.innerHTML = '<a href="https://www.google.com/search?q='+ name + '" target="_blank">' + name + '</a>'; 
+        // ごみ箱
+        td3.innerHTML = '<a onclick="removeStorage(\'' + id + '\')" class="point trash"><i class="material-icons">delete</i></a>';
+    }
+    // if(localStorage.length > 5){ // ストレージのデータが10個を超えたら
+      // tr_long = document.createElement('tr').setAttribute("colspan", "3"); //エラー
+      // tb.appendChild(tr_long); td_long = document.createElement('td'); tr_long.appendChild(td_long);
+
+      // 全て削除のごみ箱
+      // td_long.innerHTML = '<a onclick="clearStorage()">全て削除<i class="material-icons point trash">delete_forever</i></a>';
+    // }
+};
+            
+// window.onload = function() {
+//     viewStorage();
+    
+    // 削除ボタンが押されたら実行
+    // document.getElementById('clear').onclick = function() { 
+        // clearStorage();
+    // }; 
+// };
--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,20 @@
 class TopController < ApplicationController
   def index
+		# ランダムワードを生成
+		function makeWord(){
+
+			$rand_url = 'https://ja.wikipedia.org/wiki/Special:Randompage'; # Wikipediaおまかせ表示のURL
+			$headers = get_headers($rand_url, 1); # 1はヘッダーのデータを連想配列にして返す
+			$redi_url = array_pop($headers['Location']); # リダイレクト先のURL
+
+			$word = str_replace('https://ja.wikipedia.org/wiki/', '', $redi_url); # Wikipediaがランダムに探したワード
+			$word_ja = urldecode($word); # URL文字 → 読める言葉 にデコード
+
+			if(strpos($word_ja, '_') !== false){ # _（アンダーバー）が含まれる場合
+				$word_ja = strstr($word_ja, '_', true); # _（アンダーバー）以降を省略
+			}
+			return $word_ja;
+			
+		}
   end
 end
