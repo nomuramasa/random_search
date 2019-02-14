af74d3f5d719deb3b0b6119d2311930a43270a47
nommy <nomuramasam@gmail.com>
2019-02-14 15:10:12 +0900

    「特別おまかせ表示」を表示した

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,7 +1,7 @@
 class TopController < ApplicationController
   def index
 		# ランダムワードを生成
-		@uri_str = 'https://ja.wikipedia.org/wiki/Special:Randompage'
+		@rand_url = 'https://ja.wikipedia.org/wiki/Special:Randompage' # Wikipediaおまかせ表示のURL
 		# @uri_str = 'https://ja.wordpress.org'
 		require 'net/http'
 		# require 'uri'
@@ -10,8 +10,10 @@ class TopController < ApplicationController
 		  # You should choose better exception.
 		  # raise ArgumentError, 'HTTP redirect too deep' if limit == 0
 
-	  @response = Net::HTTP.get_response(URI.parse(@uri_str))
-	  @response["location"]
+	  @response = Net::HTTP.get_response(URI.parse(@rand_url))
+	  @redi_url = @response['location']
+	  @word = @redi_url.delete('https://ja.wikipedia.org/wiki/')
+	  @word_ja = URI.decode(@word)
 	  # case @response
 	  # when Net::HTTPSuccess
 	    # @response
@@ -20,5 +22,20 @@ class TopController < ApplicationController
 	  # else
 	    # @response.value
 	  # end
+
+
+	# $rand_url = 'https://ja.wikipedia.org/wiki/Special:Randompage'; # Wikipediaおまかせ表示のURL
+	# $headers = get_headers($rand_url, 1); # 1はヘッダーのデータを連想配列にして返す
+	# $redi_url = array_pop($headers['Location']); # リダイレクト先のURL
+
+	# $word = str_replace('https://ja.wikipedia.org/wiki/', '', $redi_url); # Wikipediaがランダムに探したワード
+	# $word_ja = urldecode($word); # URL文字 → 読める言葉 にデコード
+
+	# if(strpos($word_ja, '_') !== false){ # _（アンダーバー）が含まれる場合
+	# 	$word_ja = strstr($word_ja, '_', true); # _（アンダーバー）以降を省略
+	# }
+	# // return array_pop($headers['Location']);
+	# return $headers;
+
   end
 end
