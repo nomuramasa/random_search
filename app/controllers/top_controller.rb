c0a02c8232fb1ac8df03a74143b08af8e533b0d4
nommy <nomuramasam@gmail.com>
2019-02-14 14:56:03 +0900

    おまかせ表示の固定URLを出す所までは出来た

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,24 @@
 class TopController < ApplicationController
   def index
+		# ランダムワードを生成
+		@uri_str = 'https://ja.wikipedia.org/wiki/Special:Randompage'
+		# @uri_str = 'https://ja.wordpress.org'
+		require 'net/http'
+		# require 'uri'
 
+		# limit = 10s
+		  # You should choose better exception.
+		  # raise ArgumentError, 'HTTP redirect too deep' if limit == 0
+
+	  @response = Net::HTTP.get_response(URI.parse(@uri_str))
+	  @response["location"]
+	  # case @response
+	  # when Net::HTTPSuccess
+	    # @response
+	  # when Net::HTTPRedirection
+	    # @response['location']
+	  # else
+	    # @response.value
+	  # end
   end
 end
