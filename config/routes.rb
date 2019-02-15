92f69c607248315d7a4251999c6583870d22f122
nommy <nomuramasam@gmail.com>
2019-02-15 15:24:13 +0900

    ルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+
+  ####### トップページ
+
+  # 検索ワード一覧
   get '/' => 'top#index'
+
+  # 新しく追加
+  post '/create' => 'top#create'
+
+  # 削除
+  post '/:id/delete' => 'top#delete'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
