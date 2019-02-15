dd1a2c8d733f0b35adb7441e1101b6c6ef23fe78
nommy <nomuramasam@gmail.com>
2019-02-15 16:45:30 +0900

    記述を訂正&整理

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,39 +1,33 @@
 class TopController < ApplicationController
 
-	###### 検索ワード一覧ページ 
+	#### 検索ワード一覧ページ 
   def index
-
-  	if 1 == 1 # ログイン中のみ、サーバー側で単語を扱う
-
-	  	#### 本来この単語取得プログラムは、csvファイルなどから取得
-			# ランダムワードを生成  
-			@rand_url = 'https://ja.wikipedia.org/wiki/%E7%89%B9%E5%88%A5:%E3%81%8A%E3%81%BE%E3%81%8B%E3%81%9B%E8%A1%A8%E7%A4%BA' # Wikipediaおまかせ表示のURL。「https://ja.wikipedia.org/wiki/特別：おまかせ表示」と同じ。「https://ja.wikipedia.org/wiki/Special:Randompage」からのリダイレクト先
-
-			require 'net/http'
-		  @response = Net::HTTP.get_response(URI.parse(@rand_url))
-		  @redi_url = @response['location'] # 最終リダイレクト。 ランダムワードを含むURL
-		  @word = @redi_url.delete('https://ja.wikipedia.org/wiki/') # Wikipediaがランダムに探したワード
-		  @word_ja = URI.decode(@word) # URL文字 → 読める言葉 にデコード
-
-		  if @word_ja.include?('_')
-		  	@word_ja = @word_ja.sub!(/_.*/m, '') # アンダーバー_以降を削除
-		  end
-
-			#### 本来このDB挿入プログラムは、ちゃんとボタンで反応するようにして（今はページ更新時に読み込んでる）、下のaddメソッドの中に書く
-
-
-  	end
-
   end
 
-  ###### 新規にワード追加
+  #### 新規にワード追加
   def add
 
+		# ランダムワードを生成（本来はcsvファイルなどから取得） 
+		@rand_url = 'https://ja.wikipedia.org/wiki/%E7%89%B9%E5%88%A5:%E3%81%8A%E3%81%BE%E3%81%8B%E3%81%9B%E8%A1%A8%E7%A4%BA' # Wikipediaおまかせ表示のURL。「https://ja.wikipedia.org/wiki/特別：おまかせ表示」と同じ。「https://ja.wikipedia.org/wiki/Special:Randompage」からのリダイレクト先
+
+		require 'net/http'
+	  @response = Net::HTTP.get_response(URI.parse(@rand_url))
+	  @redi_url = @response['location'] # 最終リダイレクト。 ランダムワードを含むURL
+	  @word = @redi_url.delete('https://ja.wikipedia.org/wiki/') # Wikipediaがランダムに探したワード
+	  @word_ja = URI.decode(@word) # URL文字 → 読める言葉 にデコード
+
+	  if @word_ja.include?('_')
+	  	@word_ja = @word_ja.sub!(/_.*/m, '') # アンダーバー_以降を削除
+	  end
+
+	  # データベースに保存
+
+
   end
 
-  ###### 削除
-  def delete
+  #### 削除
+  # def delete
 
-  end
+  # end
 
 end
