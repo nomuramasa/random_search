3b2e15b9ad3ed382bad0addc64a5622507c56c40
nommy <nomuramasam@gmail.com>
2019-02-16 12:49:22 +0900

    新DB Userを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_15_080253) do
+ActiveRecord::Schema.define(version: 2019_02_16_034043) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "words", force: :cascade do |t|
     t.text "content"
