6b36efb9b2a73a74a05c7f5c9e65ca765384abcb
nommy <nomuramasam@gmail.com>
2019-02-16 18:17:11 +0900

    users テーブルにpasswordカラムを追加。空バリデーションも

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
 	validates :name, {presence:true} # 名前空はダメ
 	validates :email, {presence:true} # メールアドレス空はダメ
 	validates :email, {uniqueness:true} # メールアドレス重複はダメ
+	validates :password, {presence:true} # パスワード空はダメ
 
 end
