8928b479b35235fa3a2fe338807a73c4ac8a7d96
nommy <nomuramasam@gmail.com>
2019-02-16 21:07:09 +0900

    全体に適用する@current_userを定義。今からアクセス制限をする

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
-	# @current_user = User.find_by(id:[session[:user_id]])
+
+	# 最初に呼び出す
+  before_action :set_current_user
+
+  # ログイン中のユーザー
+ 	def set_current_user
+		@current_user = User.find_by(id: session[:user_id])
+	end
+
 end
