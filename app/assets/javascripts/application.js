6f3da4112e0346b9055b3e34018b255f84c96b8d
nommy <nomuramasam@gmail.com>
2019-02-20 19:07:42 +0900

    js修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => { 
+document.addEventListener('DOMContentLoaded', function(event) { 
     if (sessionStorage.getItem('visited') == null) { // sessionにvisitedがまだ無いとき（つまりこのタブで最初のアクセスのとき）
         document.getElementById('only_once').classList.add('d-block'); // 表示クラスを付ける
         // document.getElementById('only_once').classList.remove('d-none'); //消すクラスをなくす
